81e41473e648dc7806ba961c07d9b8a346c5ea96
CelestialZ_LG <1691940846@qq.com>
2025-10-08 21:14:55 +0800

    feat: unvalid print

--- src/item.cpp
+++ src/item.cpp
@@ -1,12 +1,31 @@
 #include "item.h"
 
+void unvalidPrint(string _type) {
+    cout << "No Valid" << _type << endl;
+}
+
 void Contacts::printContact() {
+    if (name != "NaN")
     cout << "Contact Name: " << name << endl;
+    else
+    unvalidPrint("Contact Name");
+    
+    if (num != "NaN")
     cout << "Contact Num: " << num << endl;
+    else 
+    unvalidPrint("Contact Name");
 }
 
 void Item::printItem() {
+    if (name != "NaN")
     cout << "Item Name:" << name << endl;
+    else
+    unvalidPrint("Item Name");
+
+    if (name != "NaN")
     cout << "Description: " << description << endl;
+    else
+    unvalidPrint("Description");
+    
     contacts.printContact();
 }
