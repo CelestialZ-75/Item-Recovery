a41adb1b965a325db704149d1ac475d249f1b5e1
CelestialZ_LG <1691940846@qq.com>
2025-10-08 21:15:26 +0800

    format

--- src/item.cpp
+++ src/item.cpp
@@ -1,31 +1,29 @@
 #include "item.h"
 
-void unvalidPrint(string _type) {
-    cout << "No Valid" << _type << endl;
-}
+void unvalidPrint(string _type) { cout << "No Valid" << _type << endl; }
 
 void Contacts::printContact() {
     if (name != "NaN")
-    cout << "Contact Name: " << name << endl;
+        cout << "Contact Name: " << name << endl;
     else
-    unvalidPrint("Contact Name");
-    
+        unvalidPrint("Contact Name");
+
     if (num != "NaN")
-    cout << "Contact Num: " << num << endl;
-    else 
-    unvalidPrint("Contact Name");
+        cout << "Contact Num: " << num << endl;
+    else
+        unvalidPrint("Contact Name");
 }
 
 void Item::printItem() {
     if (name != "NaN")
-    cout << "Item Name:" << name << endl;
+        cout << "Item Name:" << name << endl;
     else
-    unvalidPrint("Item Name");
+        unvalidPrint("Item Name");
 
     if (name != "NaN")
-    cout << "Description: " << description << endl;
+        cout << "Description: " << description << endl;
     else
-    unvalidPrint("Description");
-    
+        unvalidPrint("Description");
+
     contacts.printContact();
 }
